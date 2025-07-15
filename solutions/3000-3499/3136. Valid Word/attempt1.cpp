428cdf5d0a7e45d28cd8bec5b62c06190d5cf26a
Sarah <154954522+sarahh1201@users.noreply.github.com>
2025-07-15 15:03:22 -0700

    Create attempt1.cpp

---
+++ solutions/3000-3499/3136. Valid Word/attempt1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string word) {
+        if (word.length() < 3) {
+            return false;
+        }
+
+        bool hasVowel = false;
+        bool hasConsonant = false;
+
+        for (char ch : word) {
+            if (!isalnum(static_cast<unsigned char>(ch))) {
+                return false;
+            }
+            if (isalpha(ch)) {
+                char lowerCh = tolower(ch);
+                if (lowerCh == 'a' || lowerCh == 'e' || lowerCh == 'i' ||
+                    lowerCh == 'o' || lowerCh == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+        return hasVowel && hasConsonant;
+    }
+};
